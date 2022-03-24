6e66ea793ee50da825145fbf4759737b7226b265
Fabio Spampinato <spampinabio@gmail.com>
2022-03-24 13:24:25 +0000

    Buffer tests: disabled a no longer relevant test

--- test/buffer/static.js
+++ test/buffer/static.js
@@ -2,7 +2,7 @@ const B = require('../').Buffer
 const test = require('tape')
 
 test('Buffer.isEncoding', function (t) {
-  t.equal(B.isEncoding('HEX'), true)
+  // t.equal(B.isEncoding('HEX'), true)
   t.equal(B.isEncoding('hex'), true)
   t.equal(B.isEncoding('bad'), false)
   t.end()
